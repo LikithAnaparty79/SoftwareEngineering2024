ef5c56ad7eede4faf94f3b9cf54ed568eeb437b5
unknown <arnavkaducr7@gmail.com>
2024-10-25 21:39:03 +0530

    working changes

---
+++ FA1/FA1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IHost host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+
+        /*/ Register BlobServiceClient
+        services.AddSingleton(sp =>
+        {
+            var configuration = sp.GetRequiredService<IConfiguration>();
+            var connectionString = configuration["AzureWebJobsStorage"];
+            return new BlobServiceClient(connectionString);
+        });*/
+    })
+    .Build();
+
+        host.Run();
+    }
+}
